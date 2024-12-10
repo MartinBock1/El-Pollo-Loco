7134eb0ad6a2a6ac71172f421d84a0b694a403ff
MB-PC\ich <martinbock@arcor.de>
2024-12-10 15:28:53 +0100

    "animation for clouds, chicken, and Character"

--- assets/models/chicken.class.js
+++ assets/models/chicken.class.js
@@ -2,10 +2,18 @@ class Chicken extends MovableObject {
     y = 370;
     height = 60;
     width = 60;
+    speed = 0.5;   // Speed of the chicken's movement
 
     constructor() {
         super().loadImage('./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 200 + Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
     }
     
 }
