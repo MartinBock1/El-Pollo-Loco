e27e85340b2979b951105dec2766f2dac113d704
MB-PC\ich <martinbock@arcor.de>
2024-12-31 08:52:49 +0100

    "some fixes"

--- assets/levels/level1.js
+++ assets/levels/level1.js
@@ -3,17 +3,18 @@ function initLevel() {
 
     level1 = new Level(
         [
-            // new Chicken(),
-            // new Chicken(),
-            // new Chicken(),
-            // new Chicken(),
-            // new Chicken(),
-            // new ChickenSmall(),
-            // new ChickenSmall(),
-            // new ChickenSmall(),
-            // new ChickenSmall(),
-            // new ChickenSmall(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new ChickenSmall(),
+            new ChickenSmall(),
+            new ChickenSmall(),
+            new ChickenSmall(),
+            new ChickenSmall(),
         ],
+        
         [
             new Cloud('./assets/img/5_background/layers/4_clouds/1.png', -300, 280),
             new Cloud('./assets/img/5_background/layers/4_clouds/2.png', 300, 320),
@@ -23,29 +24,29 @@ function initLevel() {
             new Cloud('./assets/img/5_background/layers/4_clouds/2.png', 2700, 300),
         ],
 
-        [   // Create an array of background objects, each representing a layer of the background
-            new BackgroundObject('./assets/img/5_background/layers/air.png', -719),                 // insert image at x-coordinate -719
-            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/2.png', -719),     // insert image at x-coordinate -719
-            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/2.png', -719),    // insert image at x-coordinate -719
-            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/2.png', -719),     // insert image at x-coordinate -719
+        [   
+            new BackgroundObject('./assets/img/5_background/layers/air.png', -719),
+            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/2.png', -719),
+            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/2.png', -719),
+            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/2.png', -719),
 
-            new BackgroundObject('./assets/img/5_background/layers/air.png', 0),                    // insert image at x-coordinate 0
-            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 0),        // insert image at x-coordinate 0
-            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 0),       // insert image at x-coordinate 0
-            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 0),        // insert image at x-coordinate 0
-            new BackgroundObject('./assets/img/5_background/layers/air.png', 719),                  // insert image at x-coordinate 719
-            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/2.png', 719),      // insert image at x-coordinate 719
-            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/2.png', 719),     // insert image at x-coordinate 719
-            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/2.png', 719),      // insert image at x-coordinate 719
+            new BackgroundObject('./assets/img/5_background/layers/air.png', 0),
+            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 0),
+            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 0),
+            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 0),
+            new BackgroundObject('./assets/img/5_background/layers/air.png', 719),
+            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/2.png', 719),
+            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/2.png', 719),
+            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/2.png', 719),
 
-            new BackgroundObject('./assets/img/5_background/layers/air.png', 719 * 2),              // insert image at x-coordinate 0
-            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 719 * 2),  // insert image at x-coordinate 719 * 2
-            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 719 * 2), // insert image at x-coordinate 719 * 2
-            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 719 * 2),  // insert image at x-coordinate 719 * 2
-            new BackgroundObject('./assets/img/5_background/layers/air.png', 719 * 3),              // insert image at x-coordinate 719
-            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/2.png', 719 * 3),  // insert image at x-coordinate 719 * 3
-            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/2.png', 719 * 3), // insert image at x-coordinate 719 * 3
-            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/2.png', 719 * 3),  // insert image at x-coordinate 719 * 3
+            new BackgroundObject('./assets/img/5_background/layers/air.png', 719 * 2),
+            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 719 * 2),
+            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 719 * 2),
+            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 719 * 2),
+            new BackgroundObject('./assets/img/5_background/layers/air.png', 719 * 3),
+            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/2.png', 719 * 3),
+            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/2.png', 719 * 3),
+            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/2.png', 719 * 3),
         ],
 
         [
@@ -54,6 +55,11 @@ function initLevel() {
             new Bottle('./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
             new Bottle('./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png'),
             new Bottle('./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
+            new Bottle('./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png'),
+            new Bottle('./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
+            new Bottle('./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png'),
+            new Bottle('./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
+            new Bottle('./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png'),
             new Bottle('./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png'),
             new Bottle('./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
             new Bottle('./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png'),
